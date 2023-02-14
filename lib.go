72ff9dabd9fd4bf6e619398051307aaa3dedec06
tx7do <yanglinbo@gmail.com>
2023-02-14 23:16:13 +0800

    feat: add shell script.

--- lib.go
+++ lib.go
@@ -1,9 +1,9 @@
 package onnxruntime
 
 // #cgo CFLAGS: -O3 -Wall -Wno-unused-variable -Wno-deprecated-declarations -Wno-c++11-narrowing -g -Wno-sign-compare -Wno-unused-function
-// #cgo CFLAGS: -I${SRCDIR}/lib -I/opt/onnxruntime/include/ -I/usr/local/lib -I/usr/local/lib/onnxruntime/include
+// #cgo CFLAGS: -I${SRCDIR}/lib -I/opt/onnxruntime/include/ -I/usr/local/onnxruntime/include -I/usr/local/lib/onnxruntime/include
 // #cgo CXXFLAGS: -std=c++11 -g -O3 -Wno-unused-result
-// #cgo CXXFLAGS: -I${SRCDIR}/lib -I/opt/onnxruntime/include/ -I/usr/local/lib -I/usr/local/lib/onnxruntime/include
+// #cgo CXXFLAGS: -I${SRCDIR}/lib -I/opt/onnxruntime/include/ -I/usr/local/onnxruntime/include -I/usr/local/lib/onnxruntime/include
 // #cgo LDFLAGS: -lstdc++
-// #cgo LDFLAGS: -L/opt/onnxruntime/lib -L/usr/local/lib -L/usr/local/lib/onnxruntime/lib -lonnxruntime
+// #cgo LDFLAGS: -L/opt/onnxruntime/lib -L/usr/local/lib/onnxruntime/lib -L/usr/local/onnxruntime/lib -lonnxruntime
 import "C"
